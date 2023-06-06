8442190202b3c0a5d1c46132d62c4502a1779350
1homas <t@th0mas.org>
2023-06-06 08:26:39 -0700

    Updated with variables in ALL CAPS and usage description

--- ise_environment.sh
+++ ise_environment.sh
@@ -1,5 +1,8 @@
-# ISE REST API Credentials
-export ise_rest_hostname='1.2.3.4'
-export ise_rest_username='admin'
-export ise_rest_password='C1sco12345'
-export ise_verify=false
+# Example ISE REST API Credentials
+# Load them using the `source` command: source ise_environment.sh
+# Verify them using `env` for all of your environment variables
+# or print them individually using `echo $ISE_HOSTNAME`
+export ISE_HOSTNAME=1.2.3.4           # hostname or IP address of ISE PAN
+export ISE_REST_USERNAME=admin        # ISE REST API admin or operator username
+export ISE_REST_PASSWORD='ISEisC00L'  # ISE REST API admin or operator password
+export ISE_CERT_VERIFY=false          # validate the ISE certificate or not
