c5d3ada2a7075a1390dbc8e6ce9a2172f9d80031
1homas <t@th0mas.org>
2023-08-23 13:31:31 -0700

    Fixed the pipenv install

--- python_environment_install.sh
+++ python_environment_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python3 -m ensurepip --upgrade
-python3 -m pip install --upgrade pip # use a virtual development environment
-pipenv install
-pipenv install -r requirements.txt  # install required Python packages
+python3 -m pip install --upgrade pipenv # get the pipev virtual development environment
+pipenv install                          # create your virtual development environment
+pipenv install -r requirements.txt      # install required packages into enviroment
