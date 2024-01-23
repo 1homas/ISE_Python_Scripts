52184804f8b4b9d279b7da5aac75b9e8402c66ee
1homas <t@th0mas.org>
2024-01-23 14:23:51 -0800

    Added python@3.11 version

--- python_environment_install.sh
+++ python_environment_install.sh
@@ -2,5 +2,5 @@
 
 python3 -m ensurepip --upgrade
 python3 -m pip install --upgrade pipenv # get the pipev virtual development environment
-pipenv install                          # create your virtual development environment
+pipenv install python@3.11              # create your virtual development environment
 pipenv install -r requirements.txt      # install required packages into enviroment
