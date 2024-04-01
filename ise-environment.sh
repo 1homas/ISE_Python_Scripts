90f49e93c27317c12918e8962266c9bad2bb8fd8
1homas <t@th0mas.org>
2024-04-01 11:44:23 -0700

    Renamed

--- ise-environment.sh
+++
@@ -1,9 +0,0 @@
-# Example ISE REST API Credentials
-# Load them using the `source` command: source ise-env.sh
-# Verify using `env` for `echo $ISE_PPAN`
-export ISE_PPAN=1.2.3.4               # hostname or IP address of ISE Primary PAN
-export ISE_PMNT=1.2.3.4               # hostname or IP address of ISE Primary MNT
-export ISE_REST_USERNAME=admin        # ISE REST API admin or operator username
-export ISE_REST_PASSWORD='ISEisC00L'  # ISE REST API admin or operator password
-export ISE_CERT_VERIFY=false          # validate the ISE certificate or not
-export ISE_DC_PASSWORD='#DataC0nnect' # Data Connect password
