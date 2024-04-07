69f636d3c3f48a26d25b991a23e14595cf5f21aa
1homas <t@th0mas.org>
2024-04-07 08:57:31 -0700

    Added CSV,JSON,grid,text output formats and timing option

--- iseql.py
+++ iseql.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """
-
 Query ISE Data Connect using SQL on the ISE Monitoring and Troubleshooting (MNT) node.
 
 ⓘ Thin vs Thick oracledb Clients
@@ -11,9 +10,6 @@ Query ISE Data Connect using SQL on the ISE Monitoring and Troubleshooting (MNT)
   If you want to run in "thick" mode, you will need to locally install the 
   Oracle Instant Client (https://www.oracle.com/database/technologies/instant-client/downloads.html) 
   which is not currently available for the macOS ARM architecture.
-
-Usage:
-  iseql.py "{query}"
 
 Example queries (wrap them in quotes!):
   SELECT view_name FROM user_views ORDER BY view_name ASC
@@ -27,8 +23,8 @@ Example queries (wrap them in quotes!):
 
 These environment variables are required using the `export` command:
   export ISE_PMNT='1.2.3.4'             # hostname or IP address of ISE Primary MNT
-  export ISE_CERT_VERIFY=True           # Verify the ISE certificate
   export ISE_DC_PASSWORD='#DataC0nnect' # Data Connect password
+  export ISE_CERT_VERIFY=False          # Optional: Disable TLS certificate verification (allow self-signed certs)
 
 You may add these export lines to a text file and load with `source`:
   source ise-env.sh
@@ -43,22 +39,74 @@ import oracledb
 import os
 import ssl
 import sys
+import time
 import tabulate
 
 ISE_DC_PORT=2484               # Data Connect port
 ISE_DC_USERNAME='dataconnect'  # Data Connect username
 ISE_DC_SID='cpm10'             # Data Connect service name identifier
+FORMATS = ['csv', 'grid', 'json', 'table', 'text']
+
+
+def show(table:list=None, headers:list=None, format:str='text', filepath:str='-') -> None:
+    """
+    Print the table in the specified format to the file. Default: `sys.stdout` ('-').
+
+    - table (list) : a list of list items to show
+    - headers (list) : the column names for the table
+    - format (str): one the following formats:
+      - `csv`   : Show the items in a Comma-Separated Value (CSV) format
+      - `grid`  : Show the items in a table grid with borders
+      - `json`  : Show the items as a single JSON string
+      - `table` : Show the items in a text-based table
+      - `text`  : Show the items in a text-based table (no header line separator)
+    - filepath (str) : Default: `sys.stdout`
+    """
+    if table == None or len(table) <= 0: return
+
+    # 💡 Do not close sys.stdout or it may not be re-opened with multiple show() calls
+    fh = sys.stdout if filepath == '-' else open(filepath, 'w') # write to sys.stdout/terminal by default
+
+    if format == 'csv':  # CSV
+        import csv
+        writer = csv.writer(fh)
+        writer.writerow(headers)
+        writer.writerows(table)
+    elif format == 'grid': # grid
+        print(f"{tabulate.tabulate(table, headers=headers, tablefmt='simple_grid')}", file=fh)
+    elif format == 'table': # table
+        print(f"{tabulate.tabulate(table, headers=headers, tablefmt='table')}", file=fh)
+    elif format == 'json':  # JSON, one long string
+        import json
+        # must convert table values to str in case of datetime objects
+        d = {}
+        table_dicts = []
+        for row in table:
+            for header, value in zip(headers, row):
+                d[header] = str(value) 
+            table_dicts.append(d)
+        print(json.dumps({ 'table': table_dicts }), file=fh)
+    elif format == 'text':  # pretty-print
+        print(f"{tabulate.tabulate(table, headers=headers, tablefmt='plain')}", file=fh)
+    else:  # just in case something gets through the CLI parser
+        print(f"✖ Unknown format: {format}", file=sys.stderr)
+
 
 argp = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-argp.add_argument('query', help='An Oracle PL/SQL Query - wrapped in double-quotes', default=None)
-argp.add_argument('-i', '--insecure', action='store_true', default=False, help='Do not verify certificates for TLS (allow self-signed certs)')
-argp.add_argument('-v', '--verbosity', action='count', default=0, help='Verbosity; multiple allowed')
+argp.add_argument('query', help='an Oracle PL/SQL Query, wrapped in double-quotes', default=None)
+argp.add_argument('-f', '--format', choices=FORMATS, default='table')
+argp.add_argument('-i', '--insecure', action='store_true', default=False, help='do not verify certificates for TLS (allow self-signed certs)')
+argp.add_argument('-t', '--timer', action='store_true', default=False, help='show total script time' )
+argp.add_argument('-v', '--verbosity', action='count', default=0, help='verbosity; multiple allowed')
 args = argp.parse_args()
+
+if args.query is None or args.query == "": print(f"query is empty", file=sys.stderr)
+if args.timer: start_time = time.time()
 
 env = {k:v for (k, v) in os.environ.items() if k.startswith('ISE_')}  # Load environment variables
 
 ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-if args.insecure or env['ISE_CERT_VERIFY'][0:1].lower() in ['f','n']:
+if args.insecure or env.get('ISE_CERT_VERIFY', 'True')[0:1].lower() in ['f','n']:
     if args.verbosity: print("ⓘ TLS security disabled", file=sys.stderr)
     ssl_context.check_hostname = False # required before setting verify_mode == ssl.CERT_NONE
     ssl_context.verify_mode = ssl.CERT_NONE # any cert is accepted; validation errors are ignored
@@ -79,12 +127,16 @@ params = oracledb.ConnectParams(
           )
 if args.verbosity : print(f"ⓘ OracleDB Connect String: {params.get_connect_string()}", file=sys.stderr)
 
-with oracledb.connect(params=params) as connection:
-    with connection.cursor() as cursor:
-        try:
+try:
+    with oracledb.connect(params=params) as connection:
+        with connection.cursor() as cursor:
             cursor.execute(args.query)
             headers = [i[0].lower() for i in cursor.description] # description: (name, type_code, display_size, internal_size, precision, scale, null_ok)
-            rows = cursor.fetchall()
-            print(tabulate.tabulate(rows, headers=headers, tablefmt='table', showindex=False))
-        except oracledb.Error as e:
-            print(e)
+            rows = cursor.fetchall() # returns a list of tuples
+            show(table=rows, headers=headers, format=args.format)
+except Exception as e:
+    print(f"SSL: CERTIFICATE_VERIFY_FAILED. Use -i option for self-signed certificates.", file=sys.stderr)
+except oracledb.Error as e:
+    print(e, file=sys.stderr)
+
+if args.timer: print(f"⏱ {'{0:.3f}'.format(time.time() - start_time)} seconds", file=sys.stderr)
