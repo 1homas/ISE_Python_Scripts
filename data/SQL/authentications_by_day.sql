32fcd6114b25865e696801a0999fa2f5ae382d86
1homas <thomas@cisco.com>
2024-10-26 11:58:26 -0700

    Added failed_pct, total_response_time, max_response_time calculations

--- data/SQL/authentications_by_day.sql
+++ data/SQL/authentications_by_day.sql
@@ -1,7 +1,12 @@
-SELECT TRUNC(timestamp) DAY,
-  SUM(passed_count) AS Passed,
-  SUM(failed_count) AS Failed,
-  SUM(passed_count) + SUM(failed_count) as Total
+-- From the ISE Data Connect Guides' RADIUS Authentication Summary Examples
+-- https://developer.cisco.com/docs/dataconnect/guides/#radius-authentication-summary
+SELECT TRUNC(timestamp) as datetime,
+  SUM(passed_count) AS passed,
+  SUM(failed_count) AS failed,
+  SUM(passed_count) + SUM(failed_count) AS total,
+  ROUND( ( ( SUM(failed_count) / (SUM(passed_count) + SUM(failed_count)) ) * 100 ), 2 ) AS failed_pct,
+  ROUND( SUM(total_response_time) /(SUM(passed_count) + SUM(failed_count)), 2 ) AS total_response_time,
+  MAX(max_response_time) AS max_response_time
 FROM radius_authentication_summary
-GROUP BY TRUNC(timestamp)
-ORDER BY DAY DESC
+GROUP BY TRUNC(timestamp) -- defaults to day
+ORDER BY datetime ASC
