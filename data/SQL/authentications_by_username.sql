1d6816f2b44d6c565d7dcfe756fbfe3576f523d1
1homas <thomas@cisco.com>
2024-11-10 11:15:21 -0800

    Add metrics per user

--- data/SQL/authentications_by_username.sql
+++ data/SQL/authentications_by_username.sql
@@ -1,6 +1,11 @@
 SELECT username,
-  COUNT(*) AS count
-FROM radius_authentications
+  SUM(passed_count) AS passed,
+  SUM(failed_count) AS failed,
+  SUM(passed_count) + SUM(failed_count) AS total,
+  ROUND( (SUM(failed_count) / (SUM(passed_count) + SUM(failed_count)) * 100), 2) AS failed_pct,
+  ROUND(SUM(total_response_time) / (SUM(passed_count) + SUM(failed_count)), 2) AS total_response_time,
+  MAX(max_response_time) AS max_response_time
+FROM radius_authentication_summary
 GROUP BY username
-ORDER BY count DESC
+ORDER BY username ASC
 -- FETCH FIRST 10 ROWS ONLY
