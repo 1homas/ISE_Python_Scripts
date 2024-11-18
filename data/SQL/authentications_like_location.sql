4aac31ede9335f992252b621059a97d418fb93db
1homas <thomas@cisco.com>
2024-11-18 12:52:42 -0800

    Renamed to radius_auths_* to list with other RADIUS queries

--- data/SQL/authentications_like_location.sql
+++ data/SQL/authentications_like_location.sql
@@ -1,42 +1,52 @@
-SELECT *
-    -- id,
-    -- timestamp_timezone,
-    -- ise_node,
-    -- syslog_message_code,
-    -- username,
-    -- user_type,
-    -- calling_station_id,
+--
+-- RADIUS Authentications for Location like example.
+--
+-- Author: Thomas Howard, thomas@cisco.com
+-- License: MIT - https://mit-license.org
+--
+
+SELECT
+    * -- all columns
     -- access_service,
-    -- framed_ip_address,
-    -- identity_store,
-    -- identity_group,
     -- audit_session_id,
     -- authentication_method,
     -- authentication_protocol,
-    -- service_type,
+    -- authorization_profiles,
+    -- authorization_rule,
+    -- calling_station_id,
+    -- checksum,
+    -- credential_check,
     -- device_name,
     -- device_type,
+    -- endpoint_profile,
+    -- failed,
+    -- failure_reason,
+    -- framed_ip_address,
+    -- framed_ipv6_address,
+    -- id,
+    -- identity_group,
+    -- identity_store,
+    -- ise_node,
     -- location,
+    -- mdm_server_name,
     -- nas_ip_address,
+    -- nas_ipv6_address,
     -- nas_port_id,
     -- nas_port_type,
-    -- authorization_profiles,
-    -- posture_status,
-    -- security_group,
-    -- failure_reason,
-    -- response_time,
-    -- passed,
-    -- failed,
-    -- credential_check,
-    -- endpoint_profile,
-    -- mdm_server_name,
-    -- policy_set_name,
-    -- authorization_rule,
-    -- nas_ipv6_address,
-    -- framed_ipv6_address,
     -- orig_calling_station_id,
-    -- checksum,
+    -- passed,
+    -- policy_set_name,
+    -- posture_status,
+    -- response_time,
+    -- security_group,
+    -- service_type,
+    -- syslog_message_code,
     -- timestamp
+    -- timestamp_timezone,
+    -- user_type,
+    -- username,
 FROM radius_authentications
 WHERE location LIKE '%All%'
--- FETCH FIRST 10 ROWS ONLY
+-- ORDER BY timestamp ASC -- first/oldest records
+ORDER BY timestamp DESC -- most recent records
+FETCH FIRST 10 ROWS ONLY -- limit default number of rows returned for large datasets
--- data/SQL/radius_accounting_stops.sql
+++ data/SQL/radius_accounting_stops.sql
@@ -1,63 +1,79 @@
+--
+-- radius_accounting
+--
+-- 💡 Un/Comment columns to quickly customize queries. Remember the last SELECT column must not end with a `,`.
+--
+-- Author: Thomas Howard, thomas@cisco.com
+-- License: MIT - https://mit-license.org
+--
+
 SELECT
-    -- id,
-    timestamp,
-    -- event_timestamp, -- The date and time that this event occurred on the NAS
-    -- timestamp_timezone,
-    -- audit_session_id,
+    TO_CHAR(timestamp, 'YYYY-MM-DD HH24:MI:SS') AS timestamp, -- drop fractional seconds
     acct_session_id AS session_id, -- Unique numeric string identifying the server session
     acct_status_type AS status, -- Specifies whether accounting packet starts or stops a bridging, routing, or terminal server session.
     syslog_message_code AS code, -- 3000=Acct-Start, 3001=Acct-Stop, 3002=Interim-Update, 3003=Acct-On, 3004=Acct-Off
     acct_session_time AS duration, -- Length of time (in seconds) for which the session has been logged in
     calling_station_id,
     username,
-    acct_input_octets AS oct_in, -- Number of octets received during the session
-    acct_output_octets AS oct_out, -- Number of octets sent during the session
-    acct_input_packets AS pack_in, -- Number of packets received during the session
-    acct_output_packets AS pack_out, -- Number of octets sent during the session
-    -- access_service, -- ISE Allowed Protocls
-    -- acct_multi_session_id,
-    -- acct_interim_interval, -- Number of seconds between each transmittal of an interim update for a specific session
     acct_terminate_cause AS termination, -- Reason a connection was terminated
-    -- session_id, -- ⚠ very long string
-    acct_status_type AS status,
-    acct_session_time AS session_time,
-    started,
-    stopped,
-    -- framed_ip_address, -- session IP address of endpoint
-    -- framed_ipv6_address,
     device_name -- ISE network device name
-    -- nas_ip_address, -- The IP address of the NAS originating the request
-    -- nas_ipv6_address,
-    -- nas_identifier,
-    -- nas_port, -- Physical port number of the NAS (Network Access Server) originating the request
-    -- nas_port_id, -- If provided by NAS
-    -- device_groups,
-    -- ise_node, -- ISE node name
-    -- identity_store,
-    -- ad_domain,
-    -- framed_protocol,
-    -- service_type, -- RADIUS Service Type: Framed, Call-Check, etc.
-    -- response_time, -- in milliseconds
-    -- failure_reason,
-    -- security_group AS SGT, -- ⚠ empty
-    -- idle_timeout,
-    -- vn,
-    -- cisco_h323_setup_time,
-    -- cisco_h323_connect_time,
-    -- cisco_h323_disconnect_time,
-    -- security_group AS SGT, -- ⚠ empty
+    -- access_service, -- ISE Allowed Protocls
+    -- acct_authentic,
     -- acct_delay_time AS delay, -- always 0? Length of time (in seconds) for which the NAS has been sending the same accounting packet
+    -- acct_input_octets AS oct_in, -- Number of octets received during the session
+    -- acct_input_packets AS pack_in, -- Number of packets received during the session
+    -- acct_interim_interval, -- Number of seconds between each transmittal of an interim update for a specific session
+    -- acct_multi_session_id,
+    -- acct_output_octets AS oct_out, -- Number of octets sent during the session
+    -- acct_output_packets AS pack_out, -- Number of octets sent during the session
     -- acct_tunnel_connection, -- ⚠ empty
     -- acct_tunnel_packet_lost, -- ⚠ empty
-    -- service_selection_policy,
-    -- acct_authentic,
-    -- user_type,
-    -- identity_group,
+    -- ad_domain,
+    -- audit_session_id,
     -- authorization_policy,
+    -- cisco_h323_connect_time,
+    -- cisco_h323_disconnect_time,
+    -- cisco_h323_setup_time,
+    -- device_groups,
+    -- event_timestamp, -- The date and time that this event occurred on the NAS
+    -- failure_reason,
+    -- framed_ip_address, -- session IP address of endpoint
+    -- framed_ipv6_address,
+    -- framed_protocol,
+    -- id,
+    -- identity_group,
+    -- identity_store,
+    -- idle_timeout,
+    -- ise_node, -- ISE node name
+    -- nas_identifier,
+    -- nas_ip_address, -- The IP address of the NAS originating the request
+    -- nas_ipv6_address,
+    -- nas_port_id, -- If provided by NAS
+    -- nas_port, -- Physical port number of the NAS (Network Access Server) originating the request
+    -- response_time, -- in milliseconds
+    -- security_group AS SGT, -- ⚠ empty
+    -- security_group AS SGT, -- ⚠ empty
+    -- service_selection_policy,
+    -- service_type, -- RADIUS Service Type: Framed, Call-Check, etc.
+    -- session_id, -- ⚠ very long string
+    -- session_timeout -- ⚠ always empty
+    -- started, -- ⚠ always 0?
+    -- stopped, -- ⚠ always 0?
     -- termination_action,
-    -- session_timeout
+    -- timestamp_timezone,
+    -- timestamp,
+    -- user_type,
+    -- vn,
 FROM radius_accounting
 -- WHERE syslog_message_code = 3001 -- RADIUS Accounting Stop
 WHERE acct_status_type = 'Stop' -- RADIUS Accounting Stop
-ORDER BY timestamp
--- FETCH FIRST 100 ROWS ONLY
+    -- AND acct_session_time < (60*60) -- sessions < 1 hour
+    -- AND acct_session_time > 3700 -- > sessions 1 hour
+    -- AND acct_session_time > (60*60*24) -- sessions > 1 day
+    -- AND acct_session_time > (60*60*24*3) -- sessions > 3 days
+    AND TRUNC(timestamp) = TRUNC(SYSDATE) -- today
+    -- AND TRUNC(timestamp, 'HH24') = TRUNC(SYSDATE, 'HH24') -- sessions this hour
+    -- AND TRUNC(timestamp, 'MI') = TRUNC(SYSDATE, 'MI') -- sessions this minute
+-- ORDER BY timestamp ASC -- first/oldest records
+ORDER BY timestamp DESC -- most recent records
+FETCH FIRST 10 ROWS ONLY -- limit default number of rows returned for large datasets
