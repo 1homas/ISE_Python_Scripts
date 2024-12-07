bfa2db1b34e7c9e6a5daec3d1d9035c2bb3c3d14
1homas <thomas@cisco.com>
2024-12-07 11:29:53 -0800

    Added WHERE clauses and formatted timestamp to second

--- data/SQL/radius_auths_last_by_username.sql
+++ data/SQL/radius_auths_last_by_username.sql
@@ -1,13 +1,19 @@
 --
+-- Last Authentication (max timestamp) by Username
+--
 -- Author: Thomas Howard, thomas@cisco.com
 -- License: MIT - https://mit-license.org
 --
 
 SELECT
-    MAX(timestamp) AS timestamp,
+    TO_CHAR(MAX(timestamp), 'YYYY-MM-DD HH24:MI:SS') AS timestamp, -- drop fractional seconds
     username
 FROM radius_authentications
 -- WHERE username = 'thomas'
+-- WHERE timestamp > sysdate - INTERVAL '10' SECOND -- last N seconds
+-- WHERE timestamp > sysdate - INTERVAL '1' MINUTE  -- last N minutes
+-- WHERE timestamp > sysdate - INTERVAL '1' HOUR -- last N hours
+WHERE timestamp > sysdate - INTERVAL '1' DAY -- last N days
 GROUP BY username
 ORDER BY username ASC
-FETCH FIRST 50 ROWS ONLY -- limit default number of rows returned for large datasets
+-- FETCH FIRST 50 ROWS ONLY -- limit default number of rows returned for large datasets
