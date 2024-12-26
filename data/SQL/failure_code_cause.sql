11106c57b2ef590dcf63f625e148cf9477042ace
1homas <thomas@cisco.com>
2024-12-26 14:27:02 -0800

    Added column info and formatting options for long strings

--- data/SQL/failure_code_cause.sql
+++ data/SQL/failure_code_cause.sql
@@ -1,8 +1,15 @@
 --
 -- failure_code_cause
+-- 💡 Un/Comment columns to quickly customize queries. Remember the last SELECT column must not end with a `,`.
 --
 
 SELECT
-    * -- all columns
+    -- * -- all columns
+    failure_code, -- the failure code
+    -- CASE WHEN LENGTH(failure_code) > 50 THEN SUBSTR(failure_code, 1, 49) || '...' ELSE failure_code END AS failure_code, -- trim verbose messages
+    failure_cause -- the failure cause
+    -- CASE WHEN LENGTH(failure_cause) > 60 THEN SUBSTR(failure_cause, 1, 59) || '...' ELSE failure_cause END AS failure_cause -- trim verbose messages
+
 FROM failure_code_cause
-FETCH FIRST 10 ROWS ONLY -- limit default number of rows returned for large datasets
+ORDER BY failure_code ASC
+-- FETCH FIRST 10 ROWS ONLY -- limit default number of rows returned for large datasets
