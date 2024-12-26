c738a80b802c29fe9d1c7e6fb887b8215f6ee6d0
1homas <thomas@cisco.com>
2024-12-26 14:17:41 -0800

    Added column descriptions and default ORDER By

--- data/SQL/endpoint_identity_groups.sql
+++ data/SQL/endpoint_identity_groups.sql
@@ -4,11 +4,12 @@
 
 SELECT
     * -- 
-    -- id
-    -- name
-    -- description
-    -- created_by
-    -- create_time
-    -- update_time
-    -- status
+    -- id, -- database unique id
+    -- name, -- name
+    -- description, -- description
+    -- created_by, -- username
+    -- create_time, -- created TIMESTAMP(6)
+    -- update_time, -- updated TIMESTAMP(6)
+    -- status -- Active/Inactive
 FROM endpoint_identity_groups -- 
+ORDER BY name ASC
