e88cd16a1f4b360fcfd3a6490655567ade6a5ef3
1homas <thomas@cisco.com>
2024-12-31 08:27:37 -0800

    Formatted

--- ciscoisesdk-test.py
+++ ciscoisesdk-test.py
@@ -31,18 +31,18 @@ from ciscoisesdk import IdentityServicesEngineAPI
 import ciscoisesdk
 
 
-def remove_ise_ids_and_links (resources:list=[]) :
+def remove_ise_ids_and_links(resources: list = []):
     """
     Remove `id` and 'link' attributes to flatten ISE JSON data.
     """
     new_resources = []
     for r in resources:
-        key, r = r.popitem() # unwrap ISE object name
-        if type(r) == ciscoisesdk.models.mydict.MyDict :
-            if r.get('id'): 
-                del r['id']
-            if r.get('link'): 
-                del r['link']
+        key, r = r.popitem()  # unwrap ISE object name
+        if type(r) == ciscoisesdk.models.mydict.MyDict:
+            if r.get("id"):
+                del r["id"]
+            if r.get("link"):
+                del r["link"]
         new_resources.append(r)
     return new_resources
 
@@ -51,51 +51,52 @@ def main():
     """
     Entrypoint for packaged script.
     """
-    global args     # promote to global scope for use in other functions
+    global args  # promote to global scope for use in other functions
     argp = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawTextHelpFormatter)
-    argp.add_argument('-i', '--insecure', action='store_true', default=False, help='ignore cert checks')
-    argp.add_argument('-t', '--timer', action='store_true', default=False, help='show response timer' )
-    argp.add_argument('-v', '--verbose', action='store_true', default=False, help='Verbosity; multiple allowed')
+    argp.add_argument("-i", "--insecure", action="store_true", default=False, help="ignore cert checks")
+    argp.add_argument("-t", "--timer", action="store_true", default=False, help="show response timer")
+    argp.add_argument("-v", "--verbose", action="store_true", default=False, help="Verbosity; multiple allowed")
     args = argp.parse_args()
 
-    if args.verbose >= 3 : print(f'ⓘ env: {env}')
-    if args.timer :
+    if args.verbose >= 3:
+        print(f"ⓘ env: {env}")
+    if args.timer:
         global start_time
         start_time = time.time()
 
-    env = {k:v for (k,v) in os.environ.items() if k.startswith('ISE_') }  # Load Environment Variables
+    env = {k: v for (k, v) in os.environ.items() if k.startswith("ISE_")}  # Load Environment Variables
 
     ise = IdentityServicesEngineAPI(
-            base_url=f"https://{env['ISE_PPAN']}",      # IDENTITY_SERVICES_ENGINE_BASE_URL
-            username=env['ISE_USERNAME'],               # IDENTITY_SERVICES_ENGINE_USERNAME
-            password=env['ISE_PASSWORD'],               # IDENTITY_SERVICES_ENGINE_PASSWORD
-            verify=(False if env['ISE_CERT_VERIFY'][0:1].lower() in ['f','n'] else True)
-          )
+        base_url=f"https://{env['ISE_PPAN']}",  # IDENTITY_SERVICES_ENGINE_BASE_URL
+        username=env["ISE_USERNAME"],  # IDENTITY_SERVICES_ENGINE_USERNAME
+        password=env["ISE_PASSWORD"],  # IDENTITY_SERVICES_ENGINE_PASSWORD
+        verify=(False if env["ISE_CERT_VERIFY"][0:1].lower() in ["f", "n"] else True),
+    )
 
     sgts = ise.security_groups.get_security_groups()
     print(f"SGTs JSON:\n{json.dumps(sgts.response, indent=2)}", file=sys.stderr)
 
-    sgts = sgts.response['SearchResult']['resources']
+    sgts = sgts.response["SearchResult"]["resources"]
     print(f"SGT resources JSON:\n{json.dumps(sgts, indent=2)}", file=sys.stderr)
 
-    sgt_ids = list(map(lambda x: x['id'], sgts))
+    sgt_ids = list(map(lambda x: x["id"], sgts))
     print(f"SGT IDs List:\n{sgt_ids}", file=sys.stderr)
 
     # Use the IDs to get all SGT details
-    sgt_details = [ise.security_groups.get_security_group_by_id(id).response for id in sgt_ids ]
+    sgt_details = [ise.security_groups.get_security_group_by_id(id).response for id in sgt_ids]
     sgt_details = remove_ise_ids_and_links(sgt_details)
     print(f"SGT Details:\n{sgt_details}", file=sys.stderr)
 
     nads = ise.network_device.get_all()
     print(f"NADS:\n{nads.response}", file=sys.stderr)
 
-    if args.timer :
+    if args.timer:
         duration = time.time() - start_time
-        print(f'\n 🕒 {duration} seconds\n', file=sys.stderr)
+        print(f"\n 🕒 {duration} seconds\n", file=sys.stderr)
 
 
 if __name__ == "__main__":
     """
-    Entrypoint for local script.
+    Run from script
     """
     main()
